fdb53ff95800587990beaf8ed21ca85bc32d139e
nayu7u <yusuke.nadaka@gmail.com>
2022-08-28 23:45:19 +0900

    style: standardrb --fix

--- scrap.rb
+++ scrap.rb
@@ -1,28 +1,29 @@
 # typed: strict
+
 require "sorbet-runtime"
 
 class Scrap
   extend T::Sig
 
-  sig {params(v: Integer).void}
+  sig { params(v: Integer).void }
   def initialize(v)
     @v = v
   end
 
-  sig {returns(Integer)}
+  sig { returns(Integer) }
   def age
     27
   end
 
-  sig {params(x: Float).returns(String)}
+  sig { params(x: Float).returns(String) }
   def float_to_string(x)
     x.to_s
   end
 
-  sig {params(new_value: T::Boolean).void}
+  sig { params(new_value: T::Boolean).void }
   def self.set_flag(new_value)
     @flag = T.let(new_value, T.untyped)
-    p 'set'
+    p "set"
   end
 
   set_flag(true)
@@ -32,7 +33,7 @@ end
 class Simple
   extend T::Sig
 
-  sig {returns(Scrap)}
+  sig { returns(Scrap) }
   def scrap
     Scrap.new(1)
   end
@@ -42,11 +43,14 @@ class Family
   extend T::Sig
 
   class Oya; end
+
   class Ko < Oya; end
+
   class Mago < Ko; end
 
-  sig {params(x: Ko).void}
-  def self.foo(x); end
+  sig { params(x: Ko).void }
+  def self.foo(x)
+  end
 
   # foo(Oya.new)
   foo(Ko.new)
