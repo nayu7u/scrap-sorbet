400b24a9f011f6508d8c7a9571a5d90b569695d0
nayu7u <yusuke.nadaka@gmail.com>
2022-08-28 23:42:34 +0900

    learn: primitive type case

--- scrap.rb
+++ scrap.rb
@@ -8,4 +8,33 @@ class Scrap
   def initialize(v)
     @v = v
   end
+
+  sig {returns(Integer)}
+  def age
+    27
+  end
+
+  sig {params(x: Float).returns(String)}
+  def float_to_string(x)
+    x.to_s
+  end
+
+  sig {params(new_value: T::Boolean).void}
+  def self.set_flag(new_value)
+    @flag = T.let(new_value, T.untyped)
+    p 'set'
+  end
+
+  set_flag(true)
+  set_flag(false)
+end
+
+class Simple
+  extend T::Sig
+
+  sig {returns(Scrap)}
+  def scrap
+    Scrap.new(1)
+  end
+end
 end
